930a83564b8e9193313e245c0d322d489eaf6134
mat <matthew.kelcey@gmail.com>
2010-09-06 19:31:04 +1000

    trial punkt tokenizer for handling utf8. requires weird hack to remove trailing period from last token

--- noun_phrases.py
+++ noun_phrases.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
-import sys, string
+import sys, string, re
 from nltk import sent_tokenize, word_tokenize, pos_tag, RegexpParser
+from nltk.tokenize import PunktWordTokenizer
 
 grammar = "NP: {<JJ>*<NNP>+}" # 0 or more adjectives followed by 1 or more nouns (or noun phrases)
 chunk_parser = RegexpParser(grammar)
+tokenizer = PunktWordTokenizer()
+
+def remove_trailing_period(str): return re.sub(r"\.$", '', str)
 
 for line in sys.stdin.readlines():
     post_id, text = string.split(line.rstrip(), "\t")
     # print "post_id", post_id, "text", text
     for sentence in sent_tokenize(text):
-        # print "sentence", sentence
-        tagged = pos_tag(word_tokenize(sentence))
-        parse_tree = chunk_parser.parse(tagged)
-        for subtree in parse_tree.subtrees():
-            if subtree.node == 'NP':
-                phrase = subtree.leaves()
-                terms = [ term for (term,type) in phrase ]
-                print "NounPhrase", post_id, " ".join(terms)
+        #print "post_id",post_id,"sentence",sentence
+        sentence = remove_trailing_period(sentence)
+        #print "dft tokenizer words  ", word_tokenize(sentence)
+        #print "punkt tokenizer words", tokenizer.tokenize(sentence)
+        #print "dft tokenizer tags   ", pos_tag(word_tokenize(sentence))
+        #print "punkt tokenizer tags ", pos_tag(tokenizer.tokenize(sentence))
+        tagged = pos_tag(tokenizer.tokenize(sentence))
+        if not len(tagged)==0:
+            #print "tagged", tagged
+            parse_tree = chunk_parser.parse(tagged)
+            for subtree in parse_tree.subtrees():
+                if subtree.node == 'NP':
+                    phrase = subtree.leaves()
+                    terms = [ term for (term,type) in phrase ]
+                    print "NounPhrase", post_id, " ".join(terms)
 
 
 
