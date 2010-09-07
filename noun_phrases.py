46b6a4cf9bfd36a5ab6a5a054922c6d2472cbfa6
mat <matthew.kelcey@gmail.com>
2010-09-07 20:24:49 +1000

    switch to WordPunctTokenizer

--- noun_phrases.py
+++ noun_phrases.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
 import sys, string, re
 from nltk import sent_tokenize, word_tokenize, pos_tag, RegexpParser
-from nltk.tokenize import PunktWordTokenizer
+from nltk.tokenize import WordPunctTokenizer
 
 grammar = "NP: {<JJ>*<NNP>+}" # 0 or more adjectives followed by 1 or more nouns (or noun phrases)
 chunk_parser = RegexpParser(grammar)
-tokenizer = PunktWordTokenizer()
-
-def remove_trailing_period(str): return re.sub(r"\.$", '', str)
+tokenizer = WordPunctTokenizer()
 
 for line in sys.stdin.readlines():
     post_id, text = string.split(line.rstrip(), "\t")
     # print "post_id", post_id, "text", text
     for sentence in sent_tokenize(text):
         #print "post_id",post_id,"sentence",sentence
-        sentence = remove_trailing_period(sentence)
-        #print "dft tokenizer words  ", word_tokenize(sentence)
-        #print "punkt tokenizer words", tokenizer.tokenize(sentence)
-        #print "dft tokenizer tags   ", pos_tag(word_tokenize(sentence))
-        #print "punkt tokenizer tags ", pos_tag(tokenizer.tokenize(sentence))
         tagged = pos_tag(tokenizer.tokenize(sentence))
         if not len(tagged)==0:
             #print "tagged", tagged
