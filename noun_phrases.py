0c11df1bfa6f1f8a0080154e655668c28b1483b0
mat <matthew.kelcey@gmail.com>
2010-09-11 16:27:37 +1000

    force locale encoding for stdin/out to handle utf8 chars (is there a better way?)

--- noun_phrases.py
+++ noun_phrases.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
-import sys, string, re
+import sys, string, re, codecs, locale
 from nltk import sent_tokenize, word_tokenize, pos_tag, RegexpParser
-from nltk.tokenize import WordPunctTokenizer
+from nltk.tokenize import WordPunctTokenizer, PunktWordTokenizer, PunktSentenceTokenizer
 
 grammar = "NP: {<JJ>*<NNP>+}" # 0 or more adjectives followed by 1 or more nouns (or noun phrases)
 chunk_parser = RegexpParser(grammar)
-tokenizer = WordPunctTokenizer()
+sent_tokenizer = PunktSentenceTokenizer()
+
+term_tokenizer = WordPunctTokenizer()
+#term_tokenizer = PunktWordTokenizer()
+
+sys.stdin = codecs.getreader(locale.getpreferredencoding())(sys.stdin)
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 for line in sys.stdin.readlines():
-    post_id, time, text = string.split(line.rstrip(), "\t")
-    # print "post_id", post_id, "text", text
-    for sentence in sent_tokenize(text):
-        tagged = pos_tag(tokenizer.tokenize(sentence))
-        # print "post_id",post_id,"tagged",tagged
+    post_id, text = string.split(line.rstrip(), "\t")
+    #print "post_id", post_id, "text", text
+    for sentence in sent_tokenizer.tokenize(text):
+        tagged = pos_tag(term_tokenizer.tokenize(sentence))
+        #print "post_id",post_id,"tagged",tagged
         if not len(tagged)==0:
             #print "tagged", tagged
             parse_tree = chunk_parser.parse(tagged)
@@ -20,10 +26,4 @@ for line in sys.stdin.readlines():
                 if subtree.node == 'NP':
                     phrase = subtree.leaves()
                     phrase = " ".join([ term for (term,type) in phrase ])
-                    print "\t".join([post_id, time, phrase])
-
-
-
-
-
-
+                    print "\t".join([post_id,phrase])
