0ade5b398f003093389aba758ed3ff77b27f43c3
mat <matthew.kelcey@gmail.com>
2010-10-20 20:22:52 +1100

    emit errors if line has problems and continue processing rest of stream

--- noun_phrases.py
+++ noun_phrases.py
@@ -14,29 +14,34 @@ chunk_parser = RegexpParser(grammar)
 sent_tokenizer = PunktSentenceTokenizer()
 
 term_tokenizer = globals()[sys.argv[1]]()
-#term_tokenizer = WordPunctTokenizer()
+#term_tokenizer = WordPunctTokenizer() # gut feel this is better, see compare_tokenizers.sh in section stuff
 #term_tokenizer = PunktWordTokenizer()
 
 sys.stdin = codecs.getreader(locale.getpreferredencoding())(sys.stdin)
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+sys.stderr = codecs.getwriter(locale.getpreferredencoding())(sys.stderr)
 
 def remove_trailing_period(token):
     return re.sub(r'\.$', '', token)
 
 for line in sys.stdin.readlines():
-    post_id, text = string.split(line.rstrip(), "\t")
-    #print "post_id", post_id, "text", text
-    for sentence in sent_tokenizer.tokenize(text):
-        tokens = term_tokenizer.tokenize(sentence)
-        tokens_without_trailing_periods = map(remove_trailing_period, tokens) # required for PunktWordTokenizer
-        tagged = pos_tag(tokens_without_trailing_periods)
-        #print "post_id",post_id,"sentence",sentence,"tagged",tagged
-        if not len(tagged)==0:
-            #print "tagged", tagged
-            parse_tree = chunk_parser.parse(tagged)
-            for subtree in parse_tree.subtrees():
-                if subtree.node == 'NP':
-                    phrase = subtree.leaves()
-                    phrase = " ".join([ term for (term,type) in phrase ])
-                    if len(phrase) > 1: # WordPunctTokenizer leaks in stuff like ',' and ' '
-                        print "\t".join([post_id,phrase])
+    try:
+        post_id, text = string.split(line.rstrip(), "\t")
+        #print "post_id", post_id, "text", text
+        for sentence in sent_tokenizer.tokenize(text):
+            tokens = term_tokenizer.tokenize(sentence)
+            tokens_without_trailing_periods = map(remove_trailing_period, tokens) # required for PunktWordTokenizer
+            tagged = pos_tag(tokens_without_trailing_periods)
+            #print "post_id",post_id,"sentence",sentence,"tagged",tagged
+            if not len(tagged)==0:
+                #print "tagged", tagged
+                parse_tree = chunk_parser.parse(tagged)
+                for subtree in parse_tree.subtrees():
+                    if subtree.node == 'NP':
+                        phrase = subtree.leaves()
+                        phrase = " ".join([ term for (term,type) in phrase ]).strip()
+                        if len(phrase) > 1: # WordPunctTokenizer leaks in stuff like ',' and ' '
+                            print "\t".join([post_id,phrase])
+    except:
+        sys.stderr.write("error! on line ["+line+"]\n")
+
